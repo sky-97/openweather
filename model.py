671241e5267786424ba497e2e567e4fd6f5a2eaa
Akash <44522465+sky-97@users.noreply.github.com>
2022-08-16 12:52:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import requests
 def by_city():
         city = input('Enter your city name : ')
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=3cb7f658a281b611379c39ed305f441c&units=metric'.format(city)
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=apikey&units=metric'.format(city)
         res = requests.get(url)
         data = res.json()
         return data
